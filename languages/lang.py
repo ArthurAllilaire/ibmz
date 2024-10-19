11f781115f28c4f8636010e6b69ae59bdff19ef0
yuhan <yw9223@ic.ac.uk>
2024-10-19 19:05:45 +0100

    df working

--- languages/lang.py
+++ languages/lang.py
@@ -1,32 +1,17 @@
 import csv
-import pandas as pd
+import pandas as pd 
 
 languages = ['English', 'Welsh or Cymraeg', 'Gaelic (Irish)', 'Gaelic (Scottish)', 'Manx Gaelic', 'Gaelic (Not otherwise specified)', 'Cornish', 'Scots', 'Ulster Scots', 'Romany English', 'Irish Traveller Cant', 'French', 'Portuguese', 'Spanish', 'Italian', 'German', 'Polish', 'Slovak', 'Czech', 'Romanian', 'Lithuanian', 'Latvian', 'Hungarian', 'Bulgarian', 'Greek', 'Dutch', 'Swedish', 'Danish', 'Finnish', 'Estonian', 'Slovenian', 'Maltese', 'Any other European language (EU)', 'Albanian', 'Ukrainian', 'Any other Eastern European language (non EU)', 'Northern European language (non EU)', 'Bosnian, Croatian, Serbian, Montenegrin', 'Any Romani language', 'Yiddish', 'Russian', 'Turkish', 'Arabic', 'Hebrew', 'Kurdish', 'Persian or Farsi', 'Pashto', 'Any other West or Central Asian language', 'Urdu', 'Hindi', 'Panjabi', 'Pakistani Pahari (with Mirpuri and Potwari)', 'Bengali (with Sylheti and Chatgaya)', 'Gujarati', 'Marathi', 'Telugu', 'Tamil', 'Malayalam', 'Sinhala', 'Nepalese', 'Any other South Asian language', 'Mandarin Chinese', 'Cantonese Chinese', 'All other Chinese', 'Japanese', 'Korean', 'Vietnamese', 'Thai', 'Malay', 'Tagalog or Filipino', 'Any other East Asian language', 'Oceanic or Australian language', 'North or South American language', 'English-based Caribbean Creole', 'Any other Caribbean Creole', 'Amharic', 'Tigrinya', 'Somali', 'Krio', 'Akan', 'Yoruba', 'Igbo', 'Swahili or Kiswahili', 'Luganda', 'Lingala', 'Shona', 'Afrikaans', 'Any other Nigerian language', 'Any other West African language', 'Any other African language', 'British Sign Language', 'Any other sign language', 'Any sign communication system', 'Other language']
 
 def getboroughbylang (language):
     boroughswithlang = []
-    with open('newlangcensus.csv', 'r') as csvfile:
+    with open('langcensus.csv', 'r') as csvfile:
         csv_reader = csv.reader(csvfile)
         for row in csv_reader:
             if row[2] == (str(languages.index(language)+1)):
-                print('hi')
                 boroughswithlang.append([int(row[4]), row[1]])
     boroughswithlang.sort(key=lambda x: x[0], reverse=True)
     return boroughswithlang
-
-def getoasbylang(lang):
-    boroughs = getboroughbylang(lang)
-    tot = sum(borough[0] for borough in boroughs)
-    df = pd.DataFrame(columns = ['OA', 'score'])
-    for borough in boroughs:
-        if (tot == 0):
-            score = 0
-        score = borough[0] / tot
-        oas = boroughtooas(borough)
-        for oa in oas:
-            df = df.append({'OA': oa, 'score': score}, ignore_index=True)
-
-
 
 def oatoborough(oa):
     with open('summary.csv', 'r') as csvfile:
@@ -43,3 +28,25 @@ def boroughtooas(borough):
             if row[2] == borough:
                 oas.append(row[0])
     return oas
+
+def getoasbylang(lang):
+    boroughs = getboroughbylang(lang)
+    print(boroughs)
+    tot = sum(borough[0] for borough in boroughs)
+    #df = pd.DataFrame(columns = ['OA', 'score'])
+    data = []
+    for borough in boroughs:
+        if (tot == 0):
+            score = 0
+        else:
+            score = borough[0] / tot
+        oas = boroughtooas(borough[1])
+        for oa in oas:
+            data.append({'OA':oa, 'score':score})
+            # newrow = pd.DataFrame({'OA':oa, 'score':score})
+            # df = pd.concat([df, newrow], ignore_index=True)
+    df = pd.DataFrame(data)
+    return df
+
+print (getoasbylang('Mandarin Chinese'))
+print(boroughtooas(''))
